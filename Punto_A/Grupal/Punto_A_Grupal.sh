0d309dc2d37ee0872cfee8ea0517ff83181e7439
Maximiliano De La Cruz <maximilianodelacruz2000@gmail.com>
2026-06-13 21:39:13 +0000

    dev: agrega continente Asia

--- Punto_A/Grupal/Punto_A_Grupal.sh
+++ Punto_A/Grupal/Punto_A_Grupal.sh
@@ -1,2 +1,3 @@
 mkdir -p Continentes/America/{Brasil/{Sao_Paulo,Curitiba,Brasilia},Peru/{Lima,Chiclayo},Chile/Santiago}
 mkdir -p Continentes/Europa/{Espana/{Madrid,Barcelona,Valencia},Alemania/{Berlin,Munich},Italia/Roma}
+mkdir -p Continentes/Asia/{Japon/{Tokio,Osaka,Kioto},China/{Pekin,Shanghai},Corea_del_Sur/Seul}
