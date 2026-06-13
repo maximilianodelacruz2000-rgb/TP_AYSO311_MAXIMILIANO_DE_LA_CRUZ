c6b58390aadab27bb1e347e07c61c18f75f250b8
Maximiliano De La Cruz <maximilianodelacruz2000@gmail.com>
2026-06-13 21:14:30 +0000

    manager: agrega continente Europa

--- Punto_A/Grupal/Punto_A_Grupal.sh
+++ Punto_A/Grupal/Punto_A_Grupal.sh
@@ -1,1 +1,2 @@
 mkdir -p Continentes/America/{Brasil/{Sao_Paulo,Curitiba,Brasilia},Peru/{Lima,Chiclayo},Chile/Santiago}
+mkdir -p Continentes/Europa/{Espana/{Madrid,Barcelona,Valencia},Alemania/{Berlin,Munich},Italia/Roma}
