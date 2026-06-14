0ef6f664e67fd6ce954a45d1fd87d46d31f34007
Maximiliano De La Cruz <maximilianodelacruz2000@gmail.com>
2026-06-14 19:00:37 +0000

    manager: resuelve Punto C usuarios y grupos

---
+++ Punto_C/Punto_C.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+sudo groupadd g_infra
+sudo groupadd g_sysadmin
+sudo groupadd g_manager
+sudo groupadd g_dev
+
+sudo useradd -m -s /bin/bash -g g_sysadmin -G g_infra u_sysadmin
+sudo useradd -m -s /bin/bash -g g_manager -G g_infra u_manager
+sudo useradd -m -s /bin/bash -g g_dev -G g_infra u_dev
+
+# Hashes de las claves
+echo -n "u_sysadmin" | sha256sum
+echo -n "u_manager" | sha256sum
+echo -n "u_dev" | sha256sum
